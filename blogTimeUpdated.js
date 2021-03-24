7aa8d907b90fdb05ccd0a77433c6194745a36383
RayRemnant <rayremnant.me@gmail.com>
2021-03-24 10:36:24 +0100

    removed debugging console logs

--- blogTimeUpdated.js
+++ blogTimeUpdated.js
@@ -6,27 +6,20 @@ window.addEventListener("load", function () {
 	var now = new Date();
 
 	var timeDiff = -Math.abs(now - timeUpdated);
-	console.log(timeDiff + " difference");
 
 	var format = "hour";
 
 	if (timeDiff / 36e5 <= -1) {
-		console.log(timeDiff / 36e5);
 		timeDiff /= 36e5;
 	} else if (timeDiff / 60000 <= -1) {
-		console.log(timeDiff / 60000 + " minute");
 		timeDiff /= 60000;
 		format = "minute";
 	} else {
-		console.log(timeDiff + " converting in seconds...");
-		console.log(timeDiff / 1000 + " second");
 		timeDiff = timeDiff / 1000;
 		format = "second";
 	}
 
 	timeDiff = Math.ceil(timeDiff).toFixed();
-
-	console.log(timeDiff + " final");
 
 	var lang = window.location.pathname.substring(1, 3);
 
