6c24a072b614beb7e812628b247770d6e1e94051
RayRemnant <rayremnant.me@gmail.com>
2021-08-10 00:57:18 +0200

    using HOST environment variable

--- src/modules/devFetch.js
+++ src/modules/devFetch.js
@@ -1,51 +1,49 @@
 import theFetch from "theFetch";
 //fetches Janus-I and stores the data in static/data/
-export default async (fetch,
-	{ host, params, path },
-	staticPath = undefined) => {
+export default async (fetch, { params, path }, staticPath = undefined) => {
+  path = staticPath || path;
 
-	path = staticPath || path;
+  if (path.charAt(path.length - 1) == "/") {
+    path = path.substring(0, path.length - 1);
+  }
 
-	if (path.charAt(path.length - 1) == "/") {
-		path = path.substring(0, path.length - 1);
-	}
+  //console.log(path + " SERVER FETCH STARTING...");
 
-	//console.log(path + " SERVER FETCH STARTING...");
+  let data = await theFetch(
+    fetch,
+    { host: process.env.HOST, params, path },
+    staticPath
+  );
 
-	let data = await theFetch(fetch,
-		{ host, params, path },
-		staticPath);
+  //console.log(data)
 
-	//console.log(data)
+  const nodePath = require("path");
+  const fs = require("fs");
 
-	const nodePath = require("path");
-	const fs = require("fs");
+  if (process.browser === false) {
+    try {
+      const dirPath = nodePath.dirname(`static/data${path}`);
 
-	if (process.browser === false) {
-		try {
-			const dirPath = nodePath.dirname(`static/data${path}`);
+      fs.existsSync(dirPath) || fs.mkdirSync(dirPath, { recursive: true });
 
-			fs.existsSync(dirPath) || fs.mkdirSync(dirPath, { recursive: true });
+      await fs.writeFile(
+        `static/data${path}.json`,
+        `${JSON.stringify(data)}`,
+        "utf8",
+        function (err) {
+          if (err) return console.log(err);
+          console.log(`${path} WRITE OK`);
+        }
+      );
+    } catch (e) {
+      console.log(e);
+    }
+    return data;
+  }
 
-			await fs.writeFile(
-				`static/data${path}.json`,
-				`${JSON.stringify(data)}`,
-				"utf8",
-				function (err) {
-					if (err) return console.log(err);
-					console.log(`${path} WRITE OK`);
-				}
-			);
-
-		} catch (e) {
-			console.log(e)
-		}
-		return data;
-	}
-
-	console.log("FETCH FAIL");
-	let e = new Error(`${process.env.SERVER_API}/cms${path}`);
-	e.name = "NOT FOUND";
-	e.status = 404;
-	throw e;
+  console.log("FETCH FAIL");
+  let e = new Error(`${process.env.SERVER_API}/cms${path}`);
+  e.name = "NOT FOUND";
+  e.status = 404;
+  throw e;
 };
